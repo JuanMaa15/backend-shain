81b5d205eafa56bd1d5769f7256c65477edb0d09
Juan Manuel <juanmanuel.41595@gmail.com>
2025-09-14 22:22:17 -0500

    feat(cookies):  se agrego la propiedad dominio en la configuracion de las cookies

--- src/config/cookie.config.js
+++ src/config/cookie.config.js
@@ -5,6 +5,7 @@ export const configureTokenCookie = (res, token) => {
     httpOnly: true,
     secure: NODE_ENV === 'production',
     sameSite: NODE_ENV === 'production' ? 'lax' : 'strict',
+    domain: '.shain.finance',
     path: '/'
   });
 }
